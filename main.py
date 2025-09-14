2228c07a9b9e807f7e4446756db6aedd0ed9a5bb
astoko <65463088+astoko@users.noreply.github.com>
2025-09-14 20:15:00 +0100

    Lets hope...

--- main.py
+++ main.py
@@ -522,7 +522,7 @@ class ParcelWindow(Gtk.ApplicationWindow):
             license_type="GTK_LICENSE_AGPL_3_0",
             developers=["Astoko"],
             developer_name="Astoko",
-            version="0.1.1",
+            version="0.1.2",
             comments=(
                 "Parcel Buddy lets you track parcels from a wide range of carriers "
                 "with a clean, visual interface. Get real-time updates and notifications "
